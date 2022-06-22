9de09928fc94c95f41abb93c3c923979ae1f334b
DWin1 <95371458+DWin1@users.noreply.github.com>
2022-06-22 22:35:32 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-
-n = str(input('Введите число n '))
-nn = int(n + n)
-nnn = int(n + n +n)
-n = int(n)
-sum = n + nn + nnn
-print (sum)
