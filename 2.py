9c7780ea2ffe97389b0d721f2dab1680518402f0
DWin1 <95371458+DWin1@users.noreply.github.com>
2022-06-22 22:35:21 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,7 +0,0 @@
-#секунды в часы, минуты, секунды
-
-time_sec = int(input('Введите время в секундах'))
-time_hour = time_sec // 3600
-time_min = (time_sec % 3600) // 60
-time_sec = time_sec - (time_hour * 3600) - (time_min * 60)
-print (f"Ваши секунды в формате ЧЧ:ММ:СС {time_hour}:{time_min}:{time_sec}")
