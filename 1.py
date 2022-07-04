e838bd8b09a9857111038fedf4d0f889630d3a49
DWin1 <95371458+DWin1@users.noreply.github.com>
2022-07-04 23:55:34 +0300

    Исправлены 1, 3, 5 задание

---
+++ 1.py
@@ -0,0 +1,8 @@
+#работа с переменными
+
+example_var = 10
+
+example_var2 = input('Введите строку')
+
+example_var3 = int(input('Введите число'))
+print(f" example_var = {example_var}, example_var2 = {example_var2}, example_var3 = {example_var3}")
